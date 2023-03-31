6d7cfee8e7a3c34a8017b77d32c82cc3f0fc03d9
Daniel Azevedo <d.azevedo.s7@gmail.com>
2023-03-31 09:26:30 -0300

    Adicionado arquivos Ansible

--- main.tf
+++ main.tf
@@ -18,8 +18,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0735c191cf914754d"
   instance_type = "t2.micro"
   key_name      = "iac-alura"
-
+  # user_data     = <<-EOF
+  #                     #!/bin/bash
+  #                     cd /home/ubuntu
+  #                     echo "<h1>Criado com Terraform</h1>" > index.html
+  #                     nohup busybox httpd -f -p 8080 &
+  #                     EOF
+  #user_data_replace_on_change = true
   tags = {
-    Name = "Primeira Instância"
+    Name = "Terraform Ansible Python"
   }
 }
